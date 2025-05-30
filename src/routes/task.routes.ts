2c4b13836fe2c35aa3436548ab26cdc6c1867e4d
Haziel Ortiz <eortiz@inncol.com.mx>
2025-05-30 17:56:18 -0600

    feat(tasks): implement new routes

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -5,7 +5,8 @@ import {
   getTaskStrings, 
   getTaskById, 
   updateTask, 
-  deleteTask 
+  deleteTask,
+  getTaskByNumber 
 } from '../controllers/task.controller';
 
 const router = Router();
@@ -13,6 +14,7 @@ const router = Router();
 router.get('/', getTask);
 router.get('/strings', getTaskStrings);
 router.post('/', createTask);
+router.get('/number/:number', getTaskByNumber); // Nueva ruta para búsqueda por número
 router.get('/:id', getTaskById);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
